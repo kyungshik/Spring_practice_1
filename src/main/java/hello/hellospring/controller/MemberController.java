d0ee03111f8a087c6f75a41cb6785b93af5f83be
Kyungshik <kyungshikseong@gmail.com>
2023-08-03 17:03:47 +0900

    회원관리예제 - 웹 MVC 개발 : 등록

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +14,23 @@ public class MemberController {
     @Autowired //맴버 서비스를 가져다가 자동으로 연결시켜준다.
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("MemberController.create = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
